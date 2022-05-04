12b1b2d005c33f66061ed761290e9651a8eea961
unknown <vmotta95@gmail.com>
2022-05-04 15:55:23 -0300

    corrigi-e-finalizei-alguns-exercicios-lacos

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -46,35 +46,64 @@ else{
     let arrayOriginal = [10,20,55,80,92,107]
 
 //a.
+function imprimirValorDoArrayOriginal (arrayOriginal){
     for (let valor of arrayOriginal) {
         console.log(valor)
     }
-
+}
+imprimirValorDoArrayOriginal(arrayOriginal)
 //b
-for (let valor1 of arrayOriginal) {
-    console.log(valor1/10)}
+function imprimirValorDoArrayOriginal (arrayOriginal){
+for (let valor of arrayOriginal) {
+    console.log(valor/10)}
+}
 
+imprimirValorDoArrayOriginal(arrayOriginal)
 
 //c.
-    function imprimeValoresPares (array){
+    function imprimeValoresPares (arrayOriginal){
         let arrayPares =[]
             for (let i = 0; i<arrayOriginal.length; i++){
-                if (array[i] % 2 ===0) {
-                    arrayPares.push(array[i])
+                if (arrayOriginal[i] % 2 ===0) {
+                    arrayPares.push(arrayOriginal[i])
                 }
             }
             console.log(arrayPares)}
             imprimeValoresPares(arrayOriginal)
             
 //d.
-function imprimirArrayComIndex (array){
+function imprimirArrayComIndex (arrayOriginal){
     let indexArray = []
-        for(numero of indexArray){console.log ("O elemento index",arrayOriginal.indexOf(0),"e",numero)
-           
+    let i = 0
+        for(let valor of arrayOriginal){
+            indexArray.push(`O elemento index ${i} é ${valor}`)
+            i++
+        }
+           console.log(indexArray)
 }
 
 imprimirArrayComIndex(arrayOriginal)
 
 
-//e. nao consegui fazer a tempo =(
+//e. 
+function retirarMaiorNumeroOuMenorNumero (arrayOriginal){
+let numeroMaior = 0
+let numeroMenor = Infinity 
+for (let i=0; i<arrayOriginal.length;i++){
 
+    if(arrayOriginal[i]<numeroMenor){
+        numeroMenor =arrayOriginal[i]
+    }    else if(arrayOriginal[i]>numeroMaior){
+            numeroMaior= arrayOriginal[i]
+
+        }
+    }
+    console.log("O maior numero é:", numeroMaior)
+    console.log("O menor numero é:", numeroMenor)
+}
+retirarMaiorNumeroOuMenorNumero(arrayOriginal)
+
+//Oi Paty! <3
+//Refiz o exercicio com ajuda da lista do notion, ainda estou com dificuldade
+//para implementar, principalemnte esse exercicio e.
+//Mas to tentando e sei que vou conseguir! 
