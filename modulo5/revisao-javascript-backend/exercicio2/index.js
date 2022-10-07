0cb9d2e669cdcc05b54c84723db309d178a207da
unknown <vmotta95@gmail.com>
2022-10-06 21:46:26 -0300

    ex2 ok

--- modulo5/revisao-javascript-backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/exercicio2/index.js
@@ -1,1 +1,28 @@
 console.log("exercício 2");
+//Cadastro cliente
+const clientesJaCadastrados = [
+	{ id: 1, nome: "Fulano" },
+	{ id: 2, nome: "Ciclano" },
+	{ id: 3, nome: "Beltrano" },
+	{ id: 4, nome: "Fulana" }
+]
+
+const cadastraCliente = (id,nome) => {
+    let novosClientes =  [].concat(clientesJaCadastrados);
+    novosClientes.push({id,nome});
+    console.log(novosClientes)
+}
+cadastraCliente(5,"Victor");
+
+// Tabuada e validação
+tabledNumber = "5";
+
+if (tabledNumber === String(tabledNumber))
+  console.log(`Erro. Parâmetro inválido (deve ser digitado um número).`);
+else if (tabledNumber > 10)
+  console.log("Erro. Parâmetro inválido (número precisa valer entre 1 e 10).");
+else {
+  for (let i = 0; i <= 10; i++) {
+    console.log(`${i} x ${tabledNumber} = ${i * tabledNumber}`);
+  }
+}
