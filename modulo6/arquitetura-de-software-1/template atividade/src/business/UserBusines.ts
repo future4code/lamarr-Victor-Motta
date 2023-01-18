199db108ff7270ba01cf10e48ba436093f6c96b5
Victor de Oliveira da Motta <vmotta95@gmail.com>
2023-01-17 21:08:39 -0300

    feat create user

---
+++ modulo6/arquitetura-de-software-1/template atividade/src/business/UserBusines.ts
@@ -0,0 +1,39 @@
+import { UserDatabase } from "../data/UserDatabase";
+
+export class UserBusiness {
+  createUser = async (input: any): Promise<void> => {
+    try {
+      const { name, email, password } = input;
+
+      if (!name || !email || !password) {
+        throw new Error('Preencha os campos "name", "email" e "password"');
+      }
+
+      if (password.length < 6) {
+        throw new Error("Senha muito curta");
+      }
+
+      if (input.email.indexOf("@") === -1) {
+        throw new Error("Invalid Email");
+      }
+
+      const id: string = Date.now().toString();
+
+      const userDatabase = new UserDatabase();
+      await userDatabase.insertUser({
+        id,
+        name,
+        email,
+        password,
+      });
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  };
+
+  getUsers = async (input: any): Promise<void> => {
+
+  };
+
+  deleteUser = () => {};
+}
---
+++ modulo6/arquitetura-de-software-1/template atividade/src/controller/userController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { UserBusiness } from "../business/UserBusines"
+
+export class UserController {
+  createUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const input: any = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      };
+
+      const userBusiness = new UserBusiness()
+      await userBusiness.createUser(input)
+
+      res.status(201).send({ message: "Usuário criado!" });
+    } catch (error: any) {
+      res.status(400).send(error.message);
+    }
+  };
+
+  getUser = () => {};
+  deleteUser = () => {};
+}
